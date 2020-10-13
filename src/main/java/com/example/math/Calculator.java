e783962d488bc8baab7c7bf2ac16550ff68e13d9
samturton2 <72809438+samturton2@users.noreply.github.com>
2020-10-13 18:50:32 +0100

    Update Calculator.java

--- src/main/java/com/example/math/Calculator.java
+++ src/main/java/com/example/math/Calculator.java
@@ -1,6 +1,10 @@
 package com.example.math;
 
 public class Calculator {
+    
+    public int multiply(int a, int b) {
+        return a * b;
+    }
 
     public int add(int a, int b) {
         return a + b;
